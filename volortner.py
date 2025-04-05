07c41fcc87c9cc2b56ee93212d660ba4dd35525c
Albert <kharatyanalbert3@gmail.com>
2025-04-05 20:31:40 +0400

    Add File

---
+++ volortner.py
@@ -0,0 +1,80 @@
+import harcer
+#python
+def zangvac():
+
+def pajman():
+
+def cikl():
+
+def mathm():
+
+def strg():
+
+#mathem
+def hart():
+
+def tarac():
+
+def trigon():
+
+def hanr():
+
+def teorem():
+
+#patm
+def art():
+
+def arsh():
+
+def bagr():
+
+def rubin():
+
+def arajin():
+
+def erkrord():
+
+def errord():
+
+#fizika
+def astgh():
+
+def fizi():
+
+#angl
+def present():
+
+def past():
+
+def future():
+
+#sport
+def foot():
+
+def basket():
+
+def valley():
+
+def f1():
+
+def olymp():
+
+#hayoc lezu
+def jam():
+
+def exanak():
+
+def xosq():
+
+def lezup():
+
+#grak
+def tum():
+
+def abov():
+
+def mash():
+
+def nar():
+
+def sn():
