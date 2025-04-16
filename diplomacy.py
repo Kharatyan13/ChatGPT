165e7e0ed12e479415202f7df62ddd1d5a34c9f3
Albert <kharatyanalbert3@gmail.com>
2025-04-16 21:04:45 +0400

    Completed

---
+++ diplomacy.py
@@ -0,0 +1,27 @@
+# 1
+# def diplomacy1():
+#
+# def countries1():
+#
+# def union1():
+#
+# def creation1():
+#
+# 2
+# def diplomacy2():
+#
+# def countries2():
+#
+# def union2():
+#
+# def creation2():
+#
+# 3
+# def diplomacy3():
+#
+# def countries3():
+#
+# def union3():
+#
+# def creation3():
+#
---
+++ geography.py
@@ -0,0 +1,58 @@
+# #leading_countries
+# def economy():
+#
+# def manufacture():
+#
+# def population():
+#
+# def mining():
+#
+# def military():
+#
+# #continents
+# def europe():
+#
+# def asia():
+#
+# def africa():
+#
+# def oceania():
+#
+# def america():
+#
+# def antarctida():
+#
+# #race
+# def negroid():
+#
+# def europoid():
+#
+# def mongoloid():
+#
+# def ausraloid():
+#
+# #water
+# def lake():
+#
+# def sea():
+#
+# def ocean():
+#
+# def river():
+#
+# def waterfall():
+#
+# def bay():
+#
+# def canal():
+#
+# #religion
+# def christ():
+#
+# def islam():
+#
+# def huda():
+#
+# def budda():
+#
+# def israel():
